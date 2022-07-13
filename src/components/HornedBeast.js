c53dd1a6635d41cb0632fdc90ccb56d9028cc910
Hayden Cleaver <rexcleaver90@gmail.com>
2022-07-13 19:33:45 -0400

    cleaned up some code; actually completed feature #2

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,25 +1,33 @@
 import {Component} from 'react';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class HornedBeast extends Component{
     constructor(){
         super();
         
         this.state = {
-         
+         votes:0,
         }
     }
 
+    handleClick = (event) => {
+        this.setState({votes: this.state.votes+1});
+    }
 
     render(){
         return(
-            <div>
-                <h2>{this.props.keyword}</h2>
-                <img src={this.props.image_url} alt={this.props.title} title={this.props.title} width="200px"/>
-                <p>{this.props.description}</p>
-                <p>Favorited: {this.props.votes} &hearts;</p>
-                <Button variant="primary" onClick={this.props.click}>Vote for your favorite Horned Beast</Button>
-            </div>
+            <Container>
+                <Col lg={true}>
+                    <h2>{this.props.keyword}</h2>
+                    <img src={this.props.image_url} alt={this.props.title} title={this.props.title} width="200px"/>
+                    <p>{this.props.description}</p>
+                    <p>Favorited: {this.state.votes} &hearts;</p>
+                    <Button variant="primary" onClick={this.handleClick}>Vote for your favorite Horned Beast</Button>
+                </Col>
+            </Container>
         )
     }
 }
